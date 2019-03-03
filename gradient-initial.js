56297a3c511f054da4aa6b1838785749a8baeb92
Lytton <40278627+Lytton612@users.noreply.github.com>
2019-03-04 01:30:35 +0300

    background-gradient

---
+++ gradient-initial.js
@@ -0,0 +1,18 @@
+var granimInstance = new Granim({
+    element: '#canvas-basic',
+    direction: 'top-bottom',
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ["#0b0b0b", "#16222A"],
+                ["#16222A", "#3A6073"],
+                ["#240b36", "#c31432"],
+                ["#000428", "#471287"],
+                ["#a81919", "#4A569D"],
+                ["#0F2027", "#2C5364"],
+                ["#8E0E00", "#1F1C18"]
+            ]
+        }
+    }
+});
